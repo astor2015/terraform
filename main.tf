0fbc734f00089a6d4b17ecef16b1cfda907de549
EC2 Default User <ec2-user@ip-172-31-0-14.us-west-1.compute.internal>
2018-10-23 11:57:39 +0000

    It works :)

--- main.tf
+++ main.tf
@@ -9,29 +9,33 @@ provider "aws" {
 resource "aws_launch_configuration" "web" {
   image_id               = "ami-04534c96466647bfb"
   instance_type          = "t2.micro"
-  security_groups	 = ["${aws_security_group.instance.id}"]
+  security_groups        = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
-  lifecycle {
-    create_before_destroy = true
-  }
+   lifecycle {
+     create_before_destroy = true
+   }
 }
 
 resource "aws_autoscaling_group" "web" {
    launch_configuration = "${aws_security_group.instance.id}"
-   availability_zones = ["${data.aws_availability_zones.all.names}"]
+   availability_zones   = ["${data.aws_availability_zones.all.names}"]
+
    min_size = 2
    max_size = 10 
 
    tag {
-     key = "Name"
-     value = "terraform-asg-web"
-     propagete_at_launch = true
-   }
+	key = "Name"
+	value = "terraform-asg-web"
+	propagate_at_launch = true
+    }
+ }
+
+data "aws_availability_zones" "all" {}
 
 resource "aws_security_group" "instance" {
   name = "terraform-instance-web"
