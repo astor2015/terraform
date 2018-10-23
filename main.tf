a1efd62546baceb1b8fdbb94e3b899c170d1efcf
EC2 Default User <ec2-user@ip-172-31-0-14.us-west-1.compute.internal>
2018-10-23 11:16:17 +0000

    adding sec groups

--- main.tf
+++ main.tf
@@ -1,13 +1,50 @@
+terraform {
+  required_version = ">= 0.10, <= 0.11.9"
+}
+
 provider "aws" {
-region = "us-west-1"
+  region = "us-west-1"
 }
 
-resource "aws_instance" "web1" {
- ami = "ami-04534c96466647bfb"
- instance_type="t2.micro"
+resource "aws_launch_configuration" "web" {
+  image_id               = "ami-04534c96466647bfb"
+  instance_type          = "t2.micro"
+  security_groups	 = ["${aws_security_group.instance.id}"]
 
- tags {
- Name = "web1"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  lifecycle {
+    create_before_destroy = true
+  }
 }
+
+resource "aws_autoscaling_group" "web" {
+   launch_configuration = "${aws_security_group.instance.id}"
+   availability_zones = ["${data.aws_availability_zones.all.names}"]
+   min_size = 2
+   max_size = 10 
+
+   tag {
+     key = "Name"
+     value = "terraform-asg-web"
+     propagete_at_launch = true
+   }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-instance-web"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["94.26.57.152/32"]
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+
 }
 
